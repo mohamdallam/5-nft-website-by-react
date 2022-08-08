2bb44f678a399cebf42a21ccb2fa41fede8051f9
mohamdallam <mohamedahmedallam1@gmail.com>
2022-08-08 12:29:11 +0200

    hero-section

--- src/components/Ui/HeroSection.jsx
+++ src/components/Ui/HeroSection.jsx
@@ -2,6 +2,7 @@ import "./hero-section.css";
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import heroImg from "../../assets/images/hero.jpg";
 
 const HeroSection = () => {
   return (
@@ -11,7 +12,7 @@ const HeroSection = () => {
           <Col lg="6" md="6">
             <div className="hero__content">
               <h2>
-                Discover Digital art And Collect{" "}
+                Discover Digital art And Collect
                 <span> sell extraordinary</span> NFTs
               </h2>
               <p>
@@ -21,16 +22,22 @@ const HeroSection = () => {
               </p>
 
               <div className="hero__btns d-flex align-items-center gap-4">
-                <button className="hero__btns d-flex align-items-center gap-2">
+                <button className="explore__btn d-flex align-items-center gap-2">
                   <i class="ri-rocket-line"></i>
                   <Link to="/market">Explore</Link>
                 </button>
 
-                <button className="hero__btns d-flex align-items-center gap-2">
+                <button className="create__btn d-flex align-items-center gap-2">
                   <i class="ri-ball-pen-line"></i>
-                  <Link to="/create">Explore</Link>
+                  <Link to="/create">Create</Link>
                 </button>
               </div>
+            </div>
+          </Col>
+
+          <Col lg="6" md="6">
+            <div className="hero__img">
+              <img src={heroImg} alt="" className="w-100" />
             </div>
           </Col>
         </Row>
