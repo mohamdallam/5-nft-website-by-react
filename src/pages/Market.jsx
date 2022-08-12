dfb7ea931e25ccbbe33e187d4e790528beabb7e0
mohamdallam <mohamedahmedallam1@gmail.com>
2022-08-12 15:26:27 +0200

    market and nftdetails

--- src/pages/Market.jsx
+++ src/pages/Market.jsx
@@ -1,8 +1,18 @@
+import "./market.css";
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import CommonSection from "../components/Ui/Common-section/CommonSection";
 
+import NftCard from "./../components/Ui/Nft-card/NftCard";
+import { NFT__DATA } from "./../assets/data/data";
+
 const Market = () => {
+  const handleCategory = () => {};
+
+  const handleItem = () => {};
+
+  const handleSort = () => {};
+
   return (
     <>
       <CommonSection title={"Market Place"} />
@@ -10,12 +20,13 @@ const Market = () => {
       <section>
         <Container>
           <Row>
-            <Col lg="12">
-              <div className="market__product__filter">
-                <div className="filter__left">
+            <Col lg="12" className="mb-5">
+              <div className="market__product__filter d-flex align-items-center justify-content-between">
+                <div className="filter__left d-flex align-items-center gap-5">
+                  {/* select 1 All Categories */}
                   <div className="all__category__filter">
-                    <h6>All Categories</h6>
-                    <select>
+                    <select onChange={handleCategory}>
+                      <option>All Categories</option>
                       <option value="art">Art</option>
                       <option value="music">music</option>
                       <option value="domain-name">domain-name</option>
@@ -24,18 +35,33 @@ const Market = () => {
                     </select>
                   </div>
 
+                  {/* select 2 All Item*/}
                   <div className="all__item__filter">
-                    <h6>All Items</h6>
-                    <select>
-                      <option value="art">Art</option>
-                      <option value="music">music</option>
+                    <select onChange={handleItem}>
+                      <option>All Item</option>
+                      <option value="single-item">single-item</option>
+                      <option value="bundle">bundle</option>
                     </select>
                   </div>
                 </div>
 
-                <div className="filter__right"></div>
+                {/* select 3 Filter Right */}
+                <div className="filter__right">
+                  <select onChange={handleSort}>
+                    <option>Sort By</option>
+                    <option value="hight">Hight Rate</option>
+                    <option value="mid">Mid Rate</option>
+                    <option value="low">Low Rate</option>
+                  </select>
+                </div>
               </div>
             </Col>
+
+            {NFT__DATA.map((item) => (
+              <Col lg="3" md="4" sm="6" className="mb-4" key={item.id}>
+                <NftCard items={item} />
+              </Col>
+            ))}
           </Row>
         </Container>
       </section>
@@ -44,5 +70,3 @@ const Market = () => {
 };
 
 export default Market;
-
-// 14 min
--- src/pages/NftDetails.jsx
+++ src/pages/NftDetails.jsx
@@ -1,7 +1,79 @@
+import "./nftdetails.css";
 import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import CommonSection from "../components/Ui/Common-section/CommonSection";
+import { NFT__DATA } from "../assets/data/data";
+import { useParams, Link } from "react-router-dom";
+import LiveAction from "../components/Ui/2-Live-action/LiveAction";
 
 const NftDetails = () => {
-  return <div>NftDetails</div>;
+  const { id } = useParams();
+
+  const singleNft = NFT__DATA.find((item) => item.id === id);
+
+  return (
+    <>
+      <CommonSection title={singleNft.title} />
+
+      <section>
+        <Container>
+          <Row>
+            <Col lg="6" md="6" sm="6">
+              <img
+                src={singleNft.imgUrl}
+                alt=""
+                className="single__nft__img w-100"
+              />
+            </Col>
+
+            <Col lg="6" md="6" sm="6">
+              <div className="single__nft__content">
+                <h2>{singleNft.title}</h2>
+                <div className="d-flex align-items-center justify-content-between mt-4 mb-4">
+                  <div className="d-flex align-items-center gap-4 single__nft__seen">
+                    <span>
+                      <i class="ri-eye-line"> 234</i>
+                    </span>
+                    <span>
+                      <i class="ri-heart-line"></i>
+                    </span>
+                  </div>
+
+                  <div className="d-flex align-items-center gap-4 single__nft__more">
+                    <span>
+                      <i class="ri-send-plane-line"></i>
+                    </span>
+                    <span>
+                      <i class="ri-more-2-line"></i>
+                    </span>
+                  </div>
+                </div>
+              </div>
+
+              <div className="nft__creator d-flex gap-3 align-items-center ">
+                <div className="creator__img">
+                  <img src={singleNft.creatorImg} alt="" className="w-100" />
+                </div>
+
+                <div className="creator__detail">
+                  <p>Created By</p>
+                  <h6>{singleNft.creator}</h6>
+                </div>
+              </div>
+
+              <p className="my-4">{singleNft.desc}</p>
+              <button className="singleNft-btn  d-flex  align-items-center gap-2 w-100">
+                <i class="ri-shopping-bag-line"></i>
+                <Link to="/wallet">place a Bid</Link>
+              </button>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+
+      <LiveAction />
+    </>
+  );
 };
 
 export default NftDetails;
